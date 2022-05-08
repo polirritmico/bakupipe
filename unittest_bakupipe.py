40ae9fcfbbccd4afe04fa9d0e98129ee52a99bd9
polirritmico <ebray187@gmail.com>
2022-05-08 12:16:39 -0400

    Base commit

---
+++ unittest_bakupipe.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+import unittest
+import os
+
+# import classes from the script
+#from src.browser import Browser
+from bakupipe import *
+
+#@unittest.skip
+class TestBase(unittest.TestCase):
+    def test_run_command(self):
+        #self.assertIsNotNone(out)
+        #self.assertEqual(expected, out)
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
