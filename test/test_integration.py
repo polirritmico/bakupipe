bece55520821b63a5061429cf813104192ee5345
polirritmico <ebray187@gmail.com>
2022-05-15 17:53:07 -0400

    Split test code files

---
+++ test/test_integration.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+# Run with -b to hide print() output
+#from src.browser import Browser
+
+import unittest
+#import os
+
+#from bakupipe import *
+from config import *
+from src.repository import Repository
+from src.command import Command
+#from src.test import Test
+
+
+
+#@unittest.skip
+class IntegrationTests(unittest.TestCase):
+    def setUp(self):
+        self.repository = Repository()
+
+
+    # This test only work in a single branch repo
+    #@unittest.skip
+    def test_make_and_remove_branch(self):
+        test_branch = "test-branch"
+        expected_init = DEFAULT_BRANCHES_LIST
+        expected_make = expected_init.copy()
+        expected_make.append(test_branch)
+
+        self.assertCountEqual(expected_init, self.repository.get_branch_list())
+
+        self.repository.make_branch(test_branch)
+        self.assertCountEqual(expected_make, self.repository.get_branch_list())
+
+        self.repository.remove_branch(test_branch)
+        self.assertCountEqual(expected_init, self.repository.get_branch_list())
+
+
+    #@unittest.skip
+    def test_goto_branch(self):
+        print("\nExpected warning message:")
+        self.assertTrue(self.repository.goto_branch(DEFAULT_BRANCH))
+        print("\tOK\n")
+        current = self.repository.get_current_branch()
+        self.assertEqual(DEFAULT_BRANCH, current)
+
+        test_branch = "test-goto-branch"
+        self.repository.make_branch(test_branch)
+        self.repository.goto_branch(test_branch)
+
+        current = self.repository.get_current_branch()
+        self.assertEqual(test_branch, current)
+
+        self.repository.goto_branch(DEFAULT_BRANCH)
+        current = self.repository.get_current_branch()
+        self.assertEqual(DEFAULT_BRANCH, current)
+
+        self.repository.remove_branch(test_branch)
+        expected_list = DEFAULT_BRANCHES_LIST
+        current_list = self.repository.get_branch_list()
+        #self.assertEqual(expected_list, current_list)
+        self.assertCountEqual(expected_list, current_list)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
